ff522c6839e71587bcb16d0e3453134183519a07
hrv5500 <hvieillard@yahoo.fr>
2024-03-09 13:00:17 +0100

    kpiproject commit

--- kpiproject/settings.py
+++ kpiproject/settings.py
@@ -143,3 +143,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USE_THOUSAND_SEPARATOR = True
+
+FORMAT_MODULE_PATH = [
+    'kpiproject.formats',
+]
+
+
